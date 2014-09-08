055c6ffce3945e4be0d958423659123de5024f98
code6421vs <code6421vs@gmail.com>
2014-09-08 21:14:26 +0800

    first commit

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Security.Cookies;
+using Microsoft.Data.Entity;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configure(IBuilder app)
+        {
+            // Set up application services
+            app.UseServices(services =>
+            {
+                // Add MVC services to the services container
+                services.AddMvc();
+            });
+
+            // Enable Browser Link support
+            app.UseBrowserLink();
+
+            // Add static files to the request pipeline
+            app.UseStaticFiles();
+
+           
+
+            // Add MVC to the request pipeline
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default", 
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+
+                routes.MapRoute(
+                    name: "api",
+                    template: "{controller}/{id?}");
+            });
+        }
+    }
+}
